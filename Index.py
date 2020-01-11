fbb0f46533abf10b02da91d4227b2e75ecf7c639
murphya14 <aislingmurphy14@hotmail.com>
2020-01-11 11:23:27 +0000

    Changed the workouts to 1A rather than week 1 day 1 in the database for ease of use

--- Index.py
+++ Index.py
@@ -55,11 +55,7 @@ def update_excercise(week1_day1_id):
 def add_user():
     return render_template("signup.html",
     workout=mongo.db.week1_day1.find(),
-    user=mongo.db.user.find(),
-    workout_day=mongo.db.week1_day1.day.find(),
-    workout_week=mongo.db.week1_day1.week.find(),
-    user_day=mongo.db.user.day.find(),
-    user_week=mongo.db.user.week.find())
+    user=mongo.db.user.find())
 
 
 @app.route('/insert_user', methods=['POST'])
