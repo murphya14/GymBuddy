0ee8d7a3caf20ef060580f5a0eaa3ddf88c91a21
murphya14 <aislingmurphy14@hotmail.com>
2020-01-18 17:22:15 +0000

    Footer and navbar put in place

---
+++ static/main.js
@@ -0,0 +1,13 @@
+$(".complete").on("click", function() {
+    $(".complete-0").hide();    
+});
+
+$(".complete1").on("click", function() {
+    $(".complete-1").hide();    
+});
+$(".complete2").on("click", function() {
+    $(".complete-2").hide();    
+});
+$(".complete3").on("click", function() {
+    $(".complete-3").hide();    
+});
